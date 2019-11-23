d758eb1c6eb0fcc35c7e13be1d3efc5d7c24cf17
Pasha <returnto001@gmail.com>
2019-11-23 20:35:15 +0200

    Add auth module. Implement signup.

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,33 @@
+import { Repository, EntityRepository } from "typeorm";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcrypt';
+import { User } from "./user.entity";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+
+    const user = new User();
+    user.username = username;
+    user.password = await this.hashPassword(password, salt);
+    user.salt = salt;
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username alredy exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+}
